164fdff08f55b9f5a23e236a544c8e87311120d0
kennedysenyo <kennedysenyo@gmail.com>
2026-01-06 12:18:50 +0000

    Add db connection

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,18 @@
 import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+const dbURL = process.env.DATABASE_URL;
+
+if (!dbURL) {
+  throw new Error(
+    "DATBASE_URL environment variable is required to connect to db!"
+  );
+}
+
+const pool = new Pool({
+  connectionString: dbURL,
+});
 
 export const auth = betterAuth({
-  //...
+  database: pool,
 });
