0d64eec0399a9741663e4e55022e823d2f423400
kennedysenyo <kennedysenyo@gmail.com>
2026-01-07 05:48:58 +0000

    Redirect on page if no session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import LogOutButton from "@/components/logout-button";
 import { requireSession } from "@/lib/better-auth/server-auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   let user;
   const session = await requireSession();
   if (session) {
     user = session.user;
+  }
+  if (!session) {
+    redirect("/login");
   }
 
   return (
