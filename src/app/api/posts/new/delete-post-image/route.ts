b8bf29cd53bf2f762b763b00a871141443561523
kennedysenyo <kennedysenyo@gmail.com>
2026-01-08 20:30:39 +0000

    Add image delete route handler

--- src/app/api/posts/new/delete-post-image/route.ts
+++ src/app/api/posts/new/delete-post-image/route.ts
@@ -1,25 +1,19 @@
 "use server";
-import { put } from "@vercel/blob";
+
+import { del } from "@vercel/blob";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
   try {
-    const formData = await request.formData();
-    const file = formData.get("file") as File;
-    if (!file) {
-      return NextResponse.json(
-        { message: "File is required" },
-        { status: 400 }
-      );
+    const { url } = await request.json();
+    if (!url) {
+      return NextResponse.json({ message: "URL is required" }, { status: 400 });
     }
-    const date = new Date();
-    const dateString = date.toLocaleDateString().split("/").join("-");
-    const timeString = date.toUTCString().split(" ")[4].split(":").join("-");
-
-    const { url } = await put(`posts/${dateString}-${timeString}`, file, {
-      access: "public",
-    });
-    return NextResponse.json({ url }, { status: 200 });
+    await del(url);
+    return NextResponse.json(
+      { message: "Image Deleted Successfully" },
+      { status: 200 }
+    );
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ message: error.message }, { status: 500 });
---
+++ src/app/api/posts/new/upload-post-image/route.ts
@@ -0,0 +1,32 @@
+"use server";
+import { put } from "@vercel/blob";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+    if (!file) {
+      return NextResponse.json(
+        { message: "File is required" },
+        { status: 400 }
+      );
+    }
+    const date = new Date();
+    const dateString = date.toLocaleDateString().split("/").join("-");
+    const timeString = date.toUTCString().split(" ")[4].split(":").join("-");
+
+    const { url } = await put(`posts/${dateString}-${timeString}`, file, {
+      access: "public",
+    });
+    return NextResponse.json(
+      { message: "Image uploaded successfully", url },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ message: error as string }, { status: 400 });
+  }
+};
