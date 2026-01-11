e7fac1696a862a5dc06f2ab08d60c884055aac16
kennedysenyo <kennedysenyo@gmail.com>
2026-01-11 07:25:37 +0000

    Return less techy error message

--- src/utils/handle-error.ts
+++ src/utils/handle-error.ts
@@ -1,7 +1,14 @@
 export const handleError = (error: any) => {
   if (error instanceof Error) {
     console.error(error);
-    return { postId: null, errorMessage: error.message };
+    return {
+      postId: null,
+      errorMessage:
+        error.message ===
+        'duplicate key value violates unique constraint "posts_slug_key"'
+          ? "Slug Already exists, Try a different one."
+          : error.message,
+    };
   }
   return { postId: null, errorMessage: error as string };
 };
